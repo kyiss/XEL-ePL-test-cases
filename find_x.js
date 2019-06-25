eeec0790411ae7b357b04dcad704192ec2196abd
Kyiss <37474134+kyiss@users.noreply.github.com>
2019-06-25 11:02:59 +0800

    Update find_x.js

--- find_x.js
+++ find_x.js
@@ -1,3 +1,4 @@
 // input m array is stored in array "bounties"
 // storage integers of last iteration are stored in array "storages"
 console.log("A miner has just found a x: " + (bounties[0][0]>>>0));
+//console.log("A miner has just found a x: " + ((bounties[0][0]&0x0f)>>>0));   //more fast to find the x
